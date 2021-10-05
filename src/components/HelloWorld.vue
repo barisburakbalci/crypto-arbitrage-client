e4febbab442f93d08d287b6c4b200bc30131e495
Barış Burak BALCI <barisburakbalci@gmail.com>
2021-10-05 17:07:59 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="targetCompanies">
+    <table v-for="(company, companyIndex) in targetCompanies" :key="`targetCompany${companyIndex}`">
+      <tr v-for="(pair, pairIndex) in company.pairs" :key="`targetCompany${pairIndex}`">
+        <td>{{ pair.symbol }}</td>
+        <td>{{ pair.price }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  beforeCreate() {
+    this.$store.dispatch('getPriceLists');
+  },
+  computed: {
+    //sourceCompany: () => this.$store.state.sourceCompany,
+    targetCompanies() { return this.$store.state.targetCompanies }
+  }
+};
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,49 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+export default createStore({
+  state: {
+    sourceCompany: {
+      name: 'BINANCE',
+      apiURL: 'https://api.binance.com/api/v3/ticker/price',
+      priceList: [],
+    },
+    targetCompanies: [
+      {
+        name: 'BTCTURK',
+        apiURL: 'https://api.btcturk.com/api/v2/ticker',
+        priceList: [],
+      },
+      {
+        name: 'BITTURK',
+        apiURL: 'https://api.bitturk.com/v1/ticker',
+        priceList: [],
+      }
+    ],
+  },
+  getters: {},
+  actions: {
+    getPriceListOf({ commit }, company) {
+      axios.get(company.apiURL)
+        .then(res => {
+          let pairs = res.data.data ?? res.data;
+          pairs = pairs.map(pair => ({
+            symbol: pair.symbol ?? pair.pair,
+            price: pair.price ?? pair.last,
+          }));
+          commit('fillPriceList', { company, pairs });
+        })
+        .catch(err => console.warn(`${company.apiURL} adresine yapılan API isteği başarısız oldu`, err));
+    },
+    getPriceLists({ state, dispatch }) {
+      for (const company of state.targetCompanies) {
+        dispatch('getPriceListOf', company);
+      }      
+    }
+  },
+  mutations: {
+    fillPriceList(state, { company, pairs }) {
+      company.priceList = pairs;
+    }
+  },
+});
